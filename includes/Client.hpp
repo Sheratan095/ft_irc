64147ad0ce39da99b5a5163350549f3e08b93703
Sheratan095 <marco.ceccarelli.code@gmail.com>
2025-08-27 17:12:58 +0200

    todo

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -10,7 +10,7 @@ class	Client
 
 		std::string				_username;
 		std::string				_nickname;
-		std::string				_realname; // TO DO
+		std::string				_realname;
 		bool					_isAuthenticated;
 
 		std::string				_ipAddress;

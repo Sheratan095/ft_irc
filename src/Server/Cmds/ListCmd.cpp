ad4575b7ee248bb1bad94d6bc4410917df0396b1
Sheratan095 <marco.ceccarelli.code@gmail.com>
2025-09-09 09:59:04 +0200

    working on list

--- src/Server/Cmds/ListCmd.cpp
+++ src/Server/Cmds/ListCmd.cpp
@@ -2,4 +2,15 @@
 
 void	Server::listCmd(Client *client, const IRCMessage &message)
 {
+	if (client->isRegistered() == false)
+	{
+		sendResponse(client, ERR_NOTREGISTERED, "INVITE");
+		return;
+	}
+
+	if (message.parameters.size() < 1)
+	{
+		sendResponse(client, ERR_NEEDMOREPARAMS, "LIST");
+		return;
+	}
 }
